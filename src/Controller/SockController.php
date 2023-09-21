6ba0507b652799d5fd604a02213f95623f3c6c88
FOREST Benjamin <benjamin.frt28@gmail.com>
2023-09-21 13:38:31 +0200

    :sparkles: Pairs Page

--- src/Controller/SockController.php
+++ src/Controller/SockController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Pair;
 use App\Entity\Sock;
+use App\Form\PairsType;
 use App\Repository\PairRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SockRepository;
@@ -21,14 +25,28 @@ class SockController extends AbstractController
 
     #[Route('/lostsock/{id}', name: 'lost_sock', requirements: ['id' => '\d+'], methods: ['GET'])]
 
-    public function show(Sock $sock, PairRepository $pairRepository): Response
+    public function show(Sock $sock, PairRepository $pairRepository, Request $request, EntityManagerInterface $entityManager): Response
 
     {
         //$pair = $pairRepository->getRepository(Product::class)->find($id);
+        $pair= new Pair();
+        $user = $this->getUser();
+        $otherSock = $pairRepository->findOneBy(['id' => $sock->getId()]);
+
+        $form = $this->createForm(PairsType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $pair->setSock($user);
+            $pair->setotherSock($otherSock);
+
+            return $this->redirectToRoute('app_pair');
+        }
         $pattern = $sock->getPattern();
         return $this->render('/sock/profil.html.twig', [
             'sock' => $sock,
             'forme' => $pattern->getPath(),
+            'form' => $form,
         ]);
     }
 }
